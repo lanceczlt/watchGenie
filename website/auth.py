5c2f01a8bd2e2cc583822067760e046022663475
Thai Truong <thai.truong@case.edu>
2021-04-12 18:41:54 -0400

    Created html templates + login/logout/home pages

---
+++ website/auth.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return render_template("login.html")
+
+@auth.route('/sign_up')
+def sign_up():
+    return render_template("signup")
+
