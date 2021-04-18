145a3eb1e8bef6611d8189fcba8ca69b7e7ac52a
Thai Truong <thai.truong@case.edu>
2021-04-18 16:05:39 -0400

    Add user system + database creation

---
+++ website/db_connection.py
@@ -0,0 +1,13 @@
+import pymysql
+from pymysql import cursors
+
+def connect():
+    try:
+        connect = pymysql.connect(
+        user = 'root', password = 'password', host = 'localhost', database = 'movieGenie',
+        )
+        cursor = connect.cursor(pymysql.cursors.DictCursor)
+        return connect, cursor
+    except:
+        print('Could not connect to database')
+        return None
