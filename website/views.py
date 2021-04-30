d0694d7d30afadab3db470ae724ef257946b7c09
lanceczlt <82778981+lanceczlt@users.noreply.github.com>
2021-04-30 18:41:52 -0400

    updated get method for /result route

--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, request, flash, jsonify, session, redirect, url_for, session
+from db_connection import connect
 import json
 import pandas as pd 
 import numpy as np 
 
-from .db_connection import connect
 
 views = Blueprint('views', __name__)
 connect, cursor = connect()
@@ -22,7 +22,14 @@ def search():
 
 @views.route('/result', methods=['GET', 'POST'])
 def result():
-    if request.method == 'POST':
-        movie_id = request.form['movie']
+    if request.method =='GET':
+        cursor.execute(
+            "SELECT title, movie_id, tmdb_id FROM moviegenie.movies JOIN moviegenie.links WHERE moviegenie.title = %s "
+            )
+        results = cursor.fetchmany(10)
+        ld = [dict(zip(('title','movie_id','tmdb_id'), values)) for values in results]
+        print(ld)
+        return render_template('movie_results.html',movie_info= ld)
+
 
 
