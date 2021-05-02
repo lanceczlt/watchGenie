064899d86469190850bef39b327bf1f8bf46886f
Thai Truong <thai.truong@case.edu>
2021-05-02 14:13:20 -0400

    Fixing search function

--- website/db_connection.py
+++ website/db_connection.py
@@ -1,5 +1,8 @@
 import pymysql
 from pymysql import cursors
+from pymysql.cursors import DictCursor
+import json
+import requests
 
 def connect():
     try:
@@ -12,3 +15,11 @@ def connect():
         print('Could not connect to database')
         return None
 
+def get_movie_image(tmdb_id):
+    API_KEY = '?api_key=1cf50e6248dc270629e802686245c2c8'
+    BASE_URL = 'https://api.themoviedb.org/3/movie/'
+    IMG_URL = 'https://image.tmdb.org/t/p/w500'
+    searchURL = BASE_URL + str(tmdb_id) + API_KEY
+    response = requests.get(searchURL)
+    data = response.json()
+    return IMG_URL + str(data['poster_path'])
--- website/views.py
+++ website/views.py
@@ -1,12 +1,11 @@
-from flask import Blueprint, render_template, request, flash, jsonify, session, redirect, url_for, session
-from .db_connection import connect
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
+from .db_connection import connect, get_movie_image
 import json
-import pandas as pd
-import numpy as np
 
 views = Blueprint('views', __name__)
 connect, cursor = connect()
 
+search_results = []
 
 @views.route('/', methods=['GET', 'POST'])
 def landing():
@@ -20,17 +19,22 @@ def home():
 def search():
     if request.method == "POST":
         genre = request.form.get('genres')
-        release_date = request.form.get('release_date')
+        release_date = request.form.get('release_year')
         avg_vote = request.form.get('avg_vote')
-    return render_template("search.html")
+        title = request.form.get('movie_title')
+        search_query = "select * from movies JOIN movie_genre ON movies.movie_id = movie_genre.movie_id JOIN genres ON movie_genre.genre_id = genres.genre_id JOIN links ON movies.movie_id = links.movie_id WHERE genre_name = %s AND year(release_date) >= %s AND vote_average >= %s AND MATCH(title) AGAINST(%s IN NATURAL LANGUAGE MODE)"
+        cursor.execute(search_query,(genre, release_date, avg_vote, title))
+        search_results = cursor.fetchall()
+        for result in search_results:
+            result['img_url'] = get_movie_image(result['tmdb_id'])
+        session['results'] = search_results
+        print(type(session['results']))
+        return redirect(url_for('views.result'))
+    else:
+        return render_template("search.html")
 
 @views.route('/result', methods=['GET', 'POST'])
 def result():
-    if request.method =='GET':
-        cursor.execute(
-            "SELECT title, movie_id, tmdb_id FROM moviegenie.movies JOIN moviegenie.links WHERE moviegenie.title = %s "
-            )
-        results = cursor.fetchmany(10)
-        ld = [dict(zip(('title','movie_id','tmdb_id'), values)) for values in results]
-        print(ld)
-        return render_template('movie_results.html',movie_info= ld)
+    return render_template('search_result.html', search_results = session['results'])
+
+
